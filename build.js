89b0069471e7c300d2cc80bca9eb06370706d80e
Renzo Kooi <renzo.kooi@gmail.com>
2022-06-03 09:38:24 +0200

    Remove watch

--- build.js
+++ build.js
@@ -1,10 +1,7 @@
 // noinspection JSUnresolvedFunction,JSUnusedGlobalSymbols,JSUnusedLocalSymbols
 // noinspection ES6ShorthandObjectProperty
 const builder = require("esbuild");
-const dtOpts = {
-  ...(`month,day,hour,minute,second`.split(`,`).reduce( (a, v) => ({...a, [v]: `2-digit`}), {} )),
-  ...{ year: `numeric`, hour12: false,  timeZone: 'Europe/Amsterdam', } };
-const dtFormat = (dt = new Date()) => new Intl.DateTimeFormat(`nl-NL`, dtOpts).format(dt);
+
 builder.build( {
   entryPoints: ['./src/PathFinder.js'],
   bundle: true,
@@ -13,19 +10,7 @@ builder.build( {
   sourcemap: true,
   minify: true,
   format: 'esm',
-  watch: {
-    onRebuild(error, _) {
-      if (error) {
-        console.error(`${dtFormat()} watch build failed:`, error);
-      }
-      else {
-        console.log(`${dtFormat()} watch build ok`);
-      }
-    }
-  }
-} )
-  .catch((err) => {
-      console.log(err.message);
-      process.exit(1);
-    }
-  );
+} ).catch((err) => {
+  console.log(err.message);
+  process.exit(1);
+} );
